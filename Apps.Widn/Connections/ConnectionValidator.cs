6823cd1f2669e0308a4d209f87215cb647938c31
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-11-11 17:34:35 +0100

    Updated URL and added validator

--- Apps.Widn/Connections/ConnectionValidator.cs
+++ Apps.Widn/Connections/ConnectionValidator.cs
@@ -1,5 +1,9 @@
-﻿using Blackbird.Applications.Sdk.Common.Authentication;
+﻿using Apps.Widn.Api;
+using Apps.Widn.Dtos;
+using Apps.Widn.Models.Requests;
+using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
+using RestSharp;
 
 namespace Apps.Widn.Connections;
 
@@ -9,7 +13,26 @@ public class ConnectionValidator: IConnectionValidator
         IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders,
         CancellationToken cancellationToken)
     {
-        // TODO
+        var client = new WidnClient(authenticationCredentialsProviders);
+        var request = new RestRequest("/translate", Method.Post);
+        request.AddJsonBody(new
+        {
+            config = new TranslateConfig { SourceLocale = "es", TargetLocale = "en" },
+            sourceText = new List<string> { "Hola" },
+        });
+
+        try
+        {
+            var response = await client.ExecuteWithErrorHandling(request);
+        } catch (Exception ex) {
+            return new()
+            {
+                IsValid = false,
+                Message = ex.Message,
+            };
+        }
+        
+
         return new()
         {
             IsValid = true
